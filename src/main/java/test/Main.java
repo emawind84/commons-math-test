1ebb7be0606dffda3977f152736f50f7f3141406
Emanuele Disco <emawind84@gmail.com>
2016-01-21 14:35:45 +0900

    update

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,4 +1,8 @@
 package test;
+import org.apache.commons.math3.analysis.UnivariateFunction;
+import org.apache.commons.math3.analysis.interpolation.HermiteInterpolator;
+import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
+import org.apache.commons.math3.analysis.interpolation.UnivariateInterpolator;
 import org.apache.commons.math3.fitting.PolynomialCurveFitter;
 import org.apache.commons.math3.fitting.WeightedObservedPoints;
 import org.slf4j.Logger;
@@ -12,9 +16,12 @@ public class Main {
 
 		try {
 			
-			doFirstDegreeFitter();
-			doSecondDegreeFitter();
-			doThirdDegreeFitter();
+			//doFirstDegreeFitter();
+			//doSecondDegreeFitter();
+			//doThirdDegreeFitter();
+			
+			//testInterpolation();
+			testInterpolation2();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -95,5 +102,41 @@ public class Main {
 		final double[] coeff = fitter.fit(obs.toList());
 		
 		logger.debug("coeff length: {}, c1: {}, c2: {}, c3: {}, c4: {}", coeff.length, coeff[0], coeff[1], coeff[2], coeff[3]);
+	}
+	
+	private static void testInterpolation(){
+		
+		double x[] = { -8, -4, 1, 2, 5, 10 };
+		double y[] = { 64, 16, 1, 4, 25, 100};
+		
+		UnivariateInterpolator interpolator = new SplineInterpolator();
+		UnivariateFunction fun = interpolator.interpolate(x, y);
+		
+		
+		double interpolationX = 3;
+		double interpolatedY = fun.value(interpolationX);
+		
+		logger.debug("f(" + interpolationX + ") = " + interpolatedY);
+		
+	}
+	
+	private static void testInterpolation2(){
+		
+		HermiteInterpolator interpolator = new HermiteInterpolator();
+		
+		interpolator.addSamplePoint(-8.0, new double[] { 64.0 });
+		interpolator.addSamplePoint(-2.0, new double[] { 4.0 });
+		interpolator.addSamplePoint(1.0, new double[] { 1.0 });
+		interpolator.addSamplePoint(3.0, new double[] { 9.0 });
+		interpolator.addSamplePoint(5.0, new double[] { 25.0 });
+		
+		//logger.debug("value at x = 8: " + interpolator.value(8)[0]);
+		
+		// should print "interpolation polynomial: 1 + 2 x + 4 x^2 - 4 x^3 + x^4"
+		logger.debug("interpolation polynomial: " + interpolator.getPolynomials()[0]);
+		
+		for (int i = -50; i <= 50; i++) {
+			logger.debug("value at x = {}: {}", i, interpolator.value(i)[0]);
+		}
 	}
 }
